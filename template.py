b5c06fc40a69d2e7d18fa0d57e3b5fbfae76546b
Anhtt9x <anhtt454598@gmail.com>
2024-06-01 14:47:36 +0700

    new_folder

--- template.py
+++ template.py
@@ -37,9 +37,9 @@ for file_path in list_of_files:
         os.makedirs(file_dir, exist_ok=True)
         logging.info(f"Creating directory: {file_dir} for file {file_name}")
 
-    elif (not os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
-        with open(file_path, "wb") as f:
-            logging.info(f"Creating file: {file_name}")
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
+        with open(file_path, "w") as f:
+            logging.info(f"Creating file: {file_path}")
 
     else:
         logging.info("Directory already exist")
